d59be8ebd9fc5a646a74bb80ab28905bdf9405a9
helvin159 <helvin159@gmail.com>
2024-03-07 14:26:03 -0500

    minor updates

--- src/components/ProjectsGallery.js
+++ src/components/ProjectsGallery.js
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 
 const ProjectsGallery = ({ projects }) => {
 	const [totalPages, setTotalPages] = useState(2);
@@ -19,6 +20,8 @@ const ProjectsGallery = ({ projects }) => {
 	};
 
 	const [page, setPage] = useState(paginate(projects?.items, 5, 1));
+
+	// window.scrollTo(0, 0);
 
 	const nextPage = () => {
 		if (currentPage >= 1 && currentPage < totalPages) {
@@ -61,91 +64,103 @@ const ProjectsGallery = ({ projects }) => {
 									<Col
 										md={6}
 										className='projects__gallery__first__content__item__row__content'>
-										<Link
-											to={
-												path === '/'
-													? `/projects/${page[0]?.fields.slug}`
-													: page[0]?.fields.slug
-											}>
-											<img
-												src={`https:${page[0]?.fields.featuredImage.fields.file.url}`}
-												width={'100%'}
-												alt='Pic one'
-												className='img-fluid'
-											/>
-										</Link>
+										{page[0] && (
+											<Link
+												to={
+													path === '/'
+														? `/projects/${page[0]?.fields.slug}`
+														: page[0]?.fields.slug
+												}>
+												<img
+													src={`https:${page[0]?.fields.featuredImage.fields.file.url}`}
+													width={'100%'}
+													alt='Pic one'
+													className='img-fluid'
+												/>
+											</Link>
+										)}
 									</Col>
 									<Col
 										md={6}
 										className='projects__gallery__first__content__item__row__content'>
-										<Link
-											to={
-												path === '/'
-													? `/projects/${page[1]?.fields.slug}`
-													: page[1]?.fields.slug
-											}>
-											<img
-												src={`https:${page[1]?.fields.featuredImage.fields.file.url}`}
-												width={'100%'}
-												alt='Pic Two'
-												className='img-fluid'
-											/>
-										</Link>
+										{page[1] && (
+											<Link
+												to={
+													path === '/'
+														? `/projects/${page[1]?.fields.slug}`
+														: page[1]?.fields.slug
+												}>
+												<img
+													src={`https:${page[1]?.fields.featuredImage.fields.file.url}`}
+													width={'100%'}
+													alt='Pic Two'
+													className='img-fluid'
+												/>
+											</Link>
+										)}
 									</Col>
 								</Row>
 
-								<Link
-									to={
-										path === '/'
-											? `/projects/${page[2]?.fields.slug}`
-											: page[2]?.fields.slug
-									}>
-									<img
-										width={'100%'}
-										src={`https:${page[2]?.fields.featuredImage.fields.file.url}`}
-										alt='Pic Three'
-										className='img-fluid'
-									/>
-								</Link>
+								{page[2] && (
+									<Link
+										to={
+											path === '/'
+												? `/projects/${page[2]?.fields.slug}`
+												: page[2]?.fields.slug
+										}>
+										<img
+											width={'100%'}
+											src={`https:${page[2]?.fields.featuredImage.fields.file.url}`}
+											alt='Pic Three'
+											className='img-fluid'
+										/>
+									</Link>
+								)}
 							</Col>
 							<Col md={4} className='projects__gallery__first__content__item'>
-								<Link
-									to={
-										path === '/'
-											? `/projects/${page[3]?.fields.slug}`
-											: page[3]?.fields.slug
-									}>
-									<img
-										src={`https:${page[3]?.fields.featuredImage.fields.file.url}`}
-										alt='Pic Four'
-										width={'100%'}
-										className='img-fluid'
-									/>
-								</Link>
+								{page[3] && (
+									<Link
+										to={
+											path === '/'
+												? `/projects/${page[3]?.fields.slug}`
+												: page[3]?.fields.slug
+										}>
+										<img
+											src={`https:${page[3]?.fields.featuredImage.fields.file.url}`}
+											alt='Pic Four'
+											width={'100%'}
+											className='img-fluid'
+										/>
+									</Link>
+								)}
 							</Col>
 						</Row>
 					</Container>
 					<Container fluid className='projects__gallery__second px-2'>
-						<Link
-							to={
-								path === '/'
-									? `/projects/${page[4]?.fields.slug}`
-									: page[4]?.fields.slug
-							}>
-							<img
-								src={`https:${page[4]?.fields.featuredImage.fields.file.url}`}
-								alt='Pic Five'
-								width={'100%'}
-								className='img-fluid'
-							/>
-						</Link>
+						{page[3] && (
+							<Link
+								to={
+									path === '/'
+										? `/projects/${page[4]?.fields.slug}`
+										: page[4]?.fields.slug
+								}>
+								<img
+									src={`https:${page[4]?.fields.featuredImage.fields.file.url}`}
+									alt='Pic Five'
+									width={'100%'}
+									className='img-fluid'
+								/>
+							</Link>
+						)}
 					</Container>
 				</Container>
 
 				{path !== '/' && (
-					<Container>
-						<button onClick={prevPage}>prev Page</button>
-						<button onClick={nextPage}>next Page</button>
+					<Container fluid className='text-center'>
+						{currentPage > 1 && <Button onClick={prevPage}>prev Page</Button>}
+						{currentPage < totalPages && (
+							<Button onClick={nextPage}>next Page</Button>
+						)}
 					</Container>
 				)}
 			</Container>
