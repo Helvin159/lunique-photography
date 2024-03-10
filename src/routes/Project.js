b6bba4d69bbb587be662a46a0b52f5e83c09b95c
helvin159 <helvin159@gmail.com>
2024-03-10 18:40:01 -0400

    small updates

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -1,12 +1,32 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { ProjectsContext } from '../context/ProjectsContext';
 import GenericHero from '../components/GenericHero';
+import Loading from '../components/Loading';
 
 const Project = () => {
-	return (
-		<Fragment>
-			<GenericHero heading={'Projects'} subHeading={''} />
-		</Fragment>
-	);
+	const [project, setProject] = useState(null);
+	const { projects } = useContext(ProjectsContext);
+	const { slug } = useParams();
+
+	useEffect(() => {
+		if (projects) {
+			let foundProject = projects.items.find(
+				({ fields }) => fields.slug === slug
+			);
+
+			setProject(foundProject);
+		}
+	}, [projects, slug]);
+
+	if (!projects) return <Loading />;
+
+	if (projects)
+		return (
+			<Fragment>
+				<GenericHero heading={project?.fields.title} subHeading={' '} />
+			</Fragment>
+		);
 };
 
 export default Project;
