8d891970bcef219ece233125877a73045627402a
helvin159 <helvin159@gmail.com>
2024-03-10 18:13:33 -0400

    small updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,17 @@
 import React, { useContext, useRef } from 'react';
+import { useLocation } from 'react-router-dom';
+import { FooterMenuContext } from '../context/FooterContext';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
 import wavingHandIcon from '../assets/svg/icon-waving-hand.svg';
-import { FooterMenuContext } from '../context/FooterContext';
-import { Button } from 'react-bootstrap';
 
 const Footer = () => {
 	const { footerSocialMedia, footerStatement, footerContactEmail } =
 		useContext(FooterMenuContext);
+	const { pathname } = useLocation();
 
 	const form = useRef();
 
@@ -30,11 +32,13 @@ const Footer = () => {
 								</span>
 							</a>
 
-							<form ref={form}>
-								<input type='email' placeholder='Email' required />
-								<textarea placeholder='Leave me a message' />
-								<Button>Send</Button>
-							</form>
+							{!pathname.includes('contact') && (
+								<form ref={form}>
+									<input type='email' placeholder='Email' required />
+									<textarea placeholder='Leave me a message' />
+									<Button>Send</Button>
+								</form>
+							)}
 						</Col>
 						<Col md={5}>
 							<ul>
