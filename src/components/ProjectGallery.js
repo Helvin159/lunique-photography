80dcd68dbca0015f7203f5f668fd5ece97ffc06a
helvin159 <helvin159@gmail.com>
2024-03-13 17:21:03 -0400

    sets up masonry style grid for gallery page

--- src/components/ProjectGallery.js
+++ src/components/ProjectGallery.js
@@ -1,24 +1,48 @@
 import React from 'react';
-import { Col, Row } from 'react-bootstrap';
+import { Button, Col, Row } from 'react-bootstrap';
+
 import Container from 'react-bootstrap/Container';
 
 const ProjectGallery = ({ pictures }) => {
+	const btnStyle = {
+		background: 'transparent',
+		border: 'none',
+		outline: '0',
+		padding: '0',
+		margin: '0 auto',
+		minHeight: '100%',
+		height: '100%',
+	};
+
+	const handleClick = (e) => console.log(e.target.dataset.index);
 	return (
 		<Container as='section' fluid className='project-gallery'>
-			<Row>
-				{pictures?.map((i) => {
+			<Row className='project-gallery__grid'>
+				{pictures?.map((i, k) => {
 					if (!i?.fields?.file?.url) return null;
 
-					if (i?.fields?.file?.url)
+					if (i?.fields?.file?.url) {
+						const { height, width } = i?.fields?.file?.details?.image;
+
 						return (
-							<Col md={4} className='my-1' key={i?.sys?.id}>
+							<Col
+								className={`project-gallery__grid__content item ${
+									height > width
+										? 'item-tall'
+										: height < width
+										? 'item-short'
+										: 'item-med'
+								}`}
+								key={i?.sys?.id}
+								onClick={handleClick}>
 								<img
 									src={`https:${i?.fields?.file?.url}`}
+									data-index={k}
 									alt={i?.fields?.title}
-									className='img-fluid'
 								/>
 							</Col>
 						);
+					}
 
 					return null;
 				})}
