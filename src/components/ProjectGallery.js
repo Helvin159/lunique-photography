c69cd1438f729abf67b7350cc8afcecbce48bb21
helvin159 <helvin159@gmail.com>
2024-03-13 17:24:55 -0400

    removes unused vars

--- src/components/ProjectGallery.js
+++ src/components/ProjectGallery.js
@@ -1,19 +1,10 @@
 import React from 'react';
-import { Button, Col, Row } from 'react-bootstrap';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 import Container from 'react-bootstrap/Container';
 
 const ProjectGallery = ({ pictures }) => {
-	const btnStyle = {
-		background: 'transparent',
-		border: 'none',
-		outline: '0',
-		padding: '0',
-		margin: '0 auto',
-		minHeight: '100%',
-		height: '100%',
-	};
-
 	const handleClick = (e) => console.log(e.target.dataset.index);
 	return (
 		<Container as='section' fluid className='project-gallery'>
